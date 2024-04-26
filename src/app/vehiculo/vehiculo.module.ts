a1519e84b4b5a8fbd2bb9e9595c2ac60068465ef
Josue Zamir Narro Abanto <j.narro@uniandes.edu.co>
2024-04-25 20:09:57 -0500

    Importa módulo de vehículos en principal

--- src/app/vehiculo/vehiculo.module.ts
+++ src/app/vehiculo/vehiculo.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VehiculoComponent } from './vehiculo.component';
+import { VehiculoListaComponent } from './vehiculo-lista/vehiculo-lista.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [VehiculoComponent]
+  declarations: [
+    VehiculoComponent,
+    VehiculoListaComponent
+  ],
+  exports: [
+    VehiculoListaComponent
+  ]
 })
 export class VehiculoModule { }
