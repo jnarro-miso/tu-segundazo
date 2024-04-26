6b45b83abac4be4d4b2cb0c0a28eae5e3e35aec9
Josue Zamir Narro Abanto <j.narro@uniandes.edu.co>
2024-04-25 21:50:50 -0500

    Agrega contador de marcas

--- src/app/vehiculo/vehiculo-lista/vehiculo-lista.component.ts
+++ src/app/vehiculo/vehiculo-lista/vehiculo-lista.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Vehiculo } from '../vehiculo';
 import { VehiculoService } from '../vehiculo.service';
 
+type ContadorMarca = { [marca: string]: number };
+
 @Component({
   selector: 'app-vehiculo-lista',
   templateUrl: './vehiculo-lista.component.html',
@@ -9,13 +11,23 @@ import { VehiculoService } from '../vehiculo.service';
 })
 export class VehiculoListaComponent implements OnInit {
   vehiculos: Vehiculo[] = [];
+  vehiculosPorMarca: ContadorMarca = {};
 
   constructor(private vehiculoService: VehiculoService) { }
 
   obtenerVehiculos(): void {
     this.vehiculoService.obtenerVehiculos().subscribe(vehiculos => {
       this.vehiculos = vehiculos;
+
+      this.vehiculosPorMarca = vehiculos.reduce((accum: ContadorMarca, curr) => {
+        accum[curr.marca] = (accum[curr.marca] || 0) + 1;
+        return accum;
+      }, {})
     });
+  }
+
+  obtenerKeys(obj: object): string[] {
+    return Object.keys(obj);
   }
 
   ngOnInit() {
